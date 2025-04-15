8f1e94c0377dce8c2407f087c5742a0f59b19a81
Luca Morricone <morricone.luca@gmail.com>
2025-04-15 03:16:52 +0200

    chore(typography): add typography

---
+++ src/lib/ui/theme/recipes/typography.styles.ts
@@ -0,0 +1,12 @@
+const typography = {
+  heading: {
+    big: 'text-32 font-light sm:text-48',
+    medium: 'text-24 font-light sm:text-32',
+    small: 'text-20 font-bold',
+  },
+  label: 'font-medium',
+  link: 'text-link underline font-medium',
+  linked: 'text-link underline',
+} as const;
+
+export default typography;
